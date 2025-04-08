d3065959374b2dd51da87301b9c78febcdd934fe
PVS1905 <viktor66462@gmail.com>
2025-04-08 03:35:33 +0300

    Fixed flake8

--- app/main.py
+++ app/main.py
@@ -6,9 +6,12 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-def get_weather() -> None:
 
-    response = requests.get(f"https://api.weatherapi.com/v1/current.json?key={SECRET_KEY}&q=Paris&aqi=no")
+def get_weather() -> None:
+    response = requests.get(
+        f"https: //api.weatherapi.com/v1/current.json?"
+        f"key={SECRET_KEY}&q=Paris&aqi=no"
+    )
     print(response.json())
 
 
