2a4e43aa0d12d99da15d3176c98c10e0d9cf02d7
PVS1905 <viktor66462@gmail.com>
2025-04-08 03:28:15 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 def get_weather() -> None:
-    # write your code here
-    pass
+
+    response = requests.get(f"https://api.weatherapi.com/v1/current.json?key={SECRET_KEY}&q=Paris&aqi=no")
+    print(response.json())
 
 
 if __name__ == "__main__":
