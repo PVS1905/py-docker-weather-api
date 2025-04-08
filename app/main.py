8a92eadbbf55f34dc856d387b0555a823de8bdcd
PVS1905 <viktor66462@gmail.com>
2025-04-08 03:42:47 +0300

    Fixed response

--- app/main.py
+++ app/main.py
@@ -5,12 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
+API_URL = "https: //api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+AQI = "no"
 
 
 def get_weather() -> None:
     response = requests.get(
-        f"https: //api.weatherapi.com/v1/current.json?"
-        f"key={SECRET_KEY}&q=Paris&aqi=no"
+        f"https: //API_URL"
+        f"key={SECRET_KEY}&q={CITY}&aqi={AQI}"
     )
     print(response.json())
 
